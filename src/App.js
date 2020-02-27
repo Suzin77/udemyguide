40e000454974bfce0833e4eb1dce291cff0eade0
Suzin <patrycjusz.sosnowski@gmail.com>
2020-02-27 22:31:41 +0100

    add class component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Person from "./Person/Person.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    state = {
+        persons:[
+            {name:"Pat1"},
+            {name:"Pat2", age:32},
+            {name:"Pat3", age:32},
+
+        ],
+        randomAge: ()=>Math.floor(Math.random() * 30 + 1),
+    };
+
+
+    switchNameHandler = (newName) => {
+        console.log(this.state);
+        this.setState({persons:[
+                {name:"zuzu", age:Math.floor(Math.random() * 30 + 1)},
+                {name:newName, age:Math.floor(Math.random() * 30 + 1)},
+                {name:'gaga', age:Math.floor(Math.random() * 30 + 1)}
+            ]});
+    }
+
+    randomAge = () => Math.floor(Math.random() * 30 + 1);
+
+
+    render() {
+        return (
+            <div className="App">
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}
+                />
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    click = {this.switchNameHandler.bind(this, "ddduddududu")}
+                />
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}
+                />
+                <button onClick={this.switchNameHandler.bind(this,"ooooo")}>Switch Age</button>
+            </div>
+        );
+    }
 }
-
 export default App;
+
